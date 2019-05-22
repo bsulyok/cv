f78058e5e2b67880a8ce139d9d50936e7dc15b18
Bendegúz Sulyok <bsulyok94@gmail.com>
2019-05-22 12:12:23 +0200

    updating stabilizer + some documentation

--- feature_tracker.py
+++ feature_tracker.py
@@ -7,13 +7,14 @@ src = cv2.VideoCapture(parser[1])
 out_name = (parser[1].split('.'))[0]+'_feat.'+(parser[1].split('.'))[1]
 out_dim = (int(src.get(cv2.CAP_PROP_FRAME_WIDTH)), int(src.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 out = cv2.VideoWriter(out_name, cv2.VideoWriter_fourcc(*'XVID'), int(src.get(cv2.CAP_PROP_FPS)), out_dim)
-orb = cv2.ORB_create()
+detector = cv2.FastFeatureDetector_create()
+#detector = cv2.ORB_create()
 while(src.isOpened()):
     ret, frame = src.read()
     if not ret:
         break
-    kp = orb.detect(frame)
-    kp, des = orb.compute(frame, kp)
+    kp = detector.detect(frame)
+    kp, des = detector.compute(frame, kp)
     cv2.drawKeypoints(frame, kp, frame)
     out.write(frame)
 src.release()
--- stabilizer.py
+++ stabilizer.py
@@ -2,11 +2,9 @@ import cv2
 import math
 import numpy as np
 import sys
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-feature_num, feature_quality, feature_distance = 200, 0.01, 30
-
+# Reading the input video
 parser = sys.argv
 vidname, vidformat = parser[1].split('.')
 src = cv2.VideoCapture("{}.{}".format(vidname, vidformat))
@@ -14,43 +12,54 @@ frame_num, fps = int(src.get(cv2.CAP_PROP_FRAME_COUNT)), src.get(cv2.CAP_PROP_FP
 horiz, vert = src.get(cv2.CAP_PROP_FRAME_WIDTH), src.get(cv2.CAP_PROP_FRAME_HEIGHT)
 frame_size = (int(horiz), int(vert))
 
+# Initializing the descriptor extractor and matcher
+detector = cv2.ORB_create()
+matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+
+# Reading the first frame
 ret, prev = src.read()
-prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
-prev_features = cv2.goodFeaturesToTrack(1, feature_num, feature_quality, feature_distance)
-last_transform, trajectory = [], []
+gray_prev = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+kp_prev, des_prev = detector.detectAndCompute(gray_prev, None)
+
+# Iterating over the next frame, estimating the transformation between frame and previous frame
+last_trans, trajectory = None, []
 a, x, y = 0, 0, 0
-for i in tqdm(range(frame_num)):
+for frames in tqdm(range(frame_num)):
     ret, cur = src.read()
     if not ret:
         break
-    cur_gray = cv2.cvtColor(cur, cv2.COLOR_BGR2GRAY)
-    cur_features = cv2.goodFeaturesToTrack(cur_gray, feature_num, feature_quality, feature_distance)
-    cur_features, status, err = cv2.calcOpticalFlowPyrLK(prev_gray, cur_gray, prev_features, cur_features)
-    prev_tracked_features = prev_features[np.where(status == 1)[0]]
-    cur_tracked_features = cur_features[np.where(status == 1)[0]]
-    trans, ret2 = cv2.estimateAffinePartial2D(prev_tracked_features, cur_tracked_features)
+    gray_cur = cv2.cvtColor(cur, cv2.COLOR_BGR2GRAY)
+    kp_cur, des_cur = detector.detectAndCompute(gray_cur, None)
+    matches = matcher.match(des_prev, des_cur)
+    kp_1 = [kp_prev[M.queryIdx].pt for M in matches]
+    kp_2 = [kp_cur[M.trainIdx].pt for M in matches]
+    inliers = np.empty((2,3))
+    trans, ret = cv2.estimateAffinePartial2D(np.array(kp_1), np.array(kp_2), inliers, cv2.RANSAC, 3, 2000, 0.99) 
     if trans is None:
-        trans = last_transform
-    last_transform = trans
+        trans = last_trans
+    last_trans = trans
     x, y, a = x + trans[0][2], y + trans[1][2], a + np.arctan2(trans[1][0], trans[0][0])
     trajectory.append([-x, -y, -a])
-    prev, prev_gray, prev_features = cur, cur_gray, cur_features
+    gray_prev, kp_prev, des_prev = gray_cur, kp_cur, des_cur
 
+# Removing large-scale translations, leaving only the fluctuations
+fluct_trajectory, radius = [], 20
+for i in range(len(trajectory)):
+    sum_x, sum_y, sum_a, c = 0, 0, 0, 0
+    for j in range(-radius, radius):
+        if 0 <= i+j < len(trajectory):
+            sum_x, sum_y, sum_a = sum_x + trajectory[i+j][0], sum_y + trajectory[i+j][1], sum_a + trajectory[i+j][2]
+            c+=1
+    fluct_trajectory.append([trajectory[i][0]-sum_x/c, trajectory[i][1]-sum_y/c, trajectory[i][2]-sum_a/c])
+
+# Applying correction
 src.set(cv2.CAP_PROP_POS_FRAMES, 1)
 out = cv2.VideoWriter("{}_stab.{}".format(vidname, vidformat), cv2.VideoWriter_fourcc(*'XVID'), fps, frame_size)
-
-for trans in tqdm(trajectory):
+for traj in tqdm(fluct_trajectory):
     ret, cur = src.read()
     if not ret:
         break
-    cur = cv2.warpAffine(cur, np.array([[math.cos(trans[2]), -math.sin(trans[2]), trans[0]], [math.sin(trans[2]), math.cos(trans[2]), trans[1]]]), frame_size)
+    cur = cv2.warpAffine(cur, np.array([[math.cos(traj[2]), -math.sin(traj[2]), traj[0]], [math.sin(traj[2]), math.cos(traj[2]), traj[1]]]), frame_size)
     out.write(cur)
 out.release()
 src.release()
-
-with open("trajectory.txt", "w") as output:
-    for traj in trajectory:
-        output.write("{}\n".format(traj))
-
-plt.plot(trajectory)
-plt.show()
